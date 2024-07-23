e899b029e791cf26c966f9afdfe318e8ce377014
baisaarnina1208@gmail.com <baisaarnina1208@gmail.com>
2024-07-23 20:37:16 +0800

    new tabs sa indiv farm

---
+++ src/component/page/Activities.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Typography }from '@mui/material'
+const Activities = () => {
+    return(
+        <Typography>jfhksj</Typography>
+    )
+}
+export default Activities
