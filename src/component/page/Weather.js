5710bccc0c468b7aeb8eb3e28e24309b775a5e7a
baisaarnina1208@gmail.com <baisaarnina1208@gmail.com>
2024-07-26 00:59:58 +0800

    updated weather

--- src/component/page/Weather.js
+++ src/component/page/Weather.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
-import { Box } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const Weather = ({ farm }) => {
     const [weather, setWeather] = useState({
@@ -12,6 +14,8 @@ const Weather = ({ farm }) => {
         forecast: [],
         error: false,
     });
+
+    const apiKey = '3e16842f99dedf74a6cb7daabcdfafb8';
 
     const toDateFunction = (timestamp) => {
         const months = [
@@ -23,8 +27,11 @@ const Weather = ({ farm }) => {
             'Friday', 'Saturday',
         ];
         const dateObj = timestamp ? new Date(timestamp * 1000) : new Date();
-        const date = `${weekDays[dateObj.getDay()]} ${dateObj.getDate()} ${months[dateObj.getMonth()]}`;
-        return date;
+        const dayOfWeek = weekDays[dateObj.getDay()];
+        const day = dateObj.getDate();
+        const month = months[dateObj.getMonth()];
+        const year = dateObj.getFullYear();
+        return `${dayOfWeek}, ${day} ${month} ${year}`;
     };
 
     useEffect(() => {
@@ -32,7 +39,6 @@ const Weather = ({ farm }) => {
             setWeather(prevState => ({ ...prevState, loading: true }));
             const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
             const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-            const apiKey = '3e16842f99dedf74a6cb7daabcdfafb8';
             const location = `${farm.mun}, PH`;
 
             try {
@@ -52,9 +58,11 @@ const Weather = ({ farm }) => {
                     },
                 });
 
+                const dailyForecast = forecastRes.data.list.filter(item => new Date(item.dt_txt).getHours() === 12);
+
                 setWeather({
                     current: currentWeatherRes.data,
-                    forecast: forecastRes.data.list,
+                    forecast: dailyForecast,
                     loading: false,
                     error: false,
                 });
@@ -72,10 +80,10 @@ const Weather = ({ farm }) => {
             width: '100%',
             height: '100%',
             display: 'flex',
-            boxShadow: 2,
-            backgroundColor: '#fff',
+            // boxShadow: 2,
+            // backgroundColor: '#fff',
             padding: 2,
-            borderRadius: 4
+            // borderRadius: 4
         }}>
             {weather.loading && (
                 <>
@@ -95,50 +103,82 @@ const Weather = ({ farm }) => {
                 </>
             )}
             {weather.current.main && (
-                <Box sx={{
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 1.5,
-                    justifyContent: 'center'
-                }}>
-                    <Box sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: '#88C488',
-                        padding: 2,
-                        borderRadius: 4
-                    }}>
-                        <div className="city-name">
-                            <h2>
-                                {weather.current.name}, Camarines Norte 
-                            </h2>
-                        </div>
-                        <div className="date">
-                            <span>{toDateFunction()}</span>
-                        </div>
-                        <div className="icon-temp">
-                            <img
-                                className=""
-                                src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
-                                alt={weather.current.weather[0].description}
-                            />
-                            {Math.round(weather.current.main.temp)}
-                            <sup className="deg">°C</sup>
-                        </div>
-                        <div className="des-wind">
-                            <p>{weather.current.weather[0].description.toUpperCase()}</p>
-                            <p>Wind Speed: {weather.current.wind.speed} m/s</p>
-                        </div>
-                    </Box>
-                    <div className="forecast">
-                        <h3>5-Day Forecast (3-hour intervals)</h3>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, overflowX: 'scroll' }}>
-                            {weather.forecast.slice(0, 40).map((item, index) => (
-                                <Box key={index} className="forecast-item" sx={{ textAlign: 'center', minWidth: '20%', backgroundColor:'#88C488' , borderRadius:4, padding:5}}>
+                <Grid container >
+
+                    <Grid item xs={12} lg={12} >
+                        <Box sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            flexDirection: 'row',
+                            boxShadow: 2,
+                            backgroundColor: '#28B463',
+                            gap: 5,
+                            justifyContent: 'space-evenly'
+                        }}>
+                            <Box sx={{}}>
+                                <img
+                                    className=""
+                                    src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
+                                    alt={weather.current.weather[0].description}
+                                    width='100%'
+                                    height='200px'
+                                />
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <Typography variant='h3' sx={{ marginLeft: 5, color: 'white' }}>
+                                    {Math.round(weather.current.main.temp)}
+                                    <sup className="deg">°C</sup>
+                                </Typography>
+                                <Typography variant='h6' sx={{ color: 'white' }}>
+                                    {weather.current.weather[0].description.toUpperCase()}
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                                    <img src={require('../image_src/pin.png')} width='20px' height='20px' />
+                                    <Typography variant='button' sx={{ color: 'white' }}>
+                                        {weather.current.name}, Camarines Norte
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                                    <img src={require('../image_src/calendar.png')} width='20px' height='20px' />
+                                    <Typography variant='button' sx={{ color: 'white' }}>
+                                        {toDateFunction()}
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                                    <img src={require('../image_src/wind.png')} width='20px' height='20px' />
+                                    <Typography variant='button' sx={{ color: 'white' }}>
+                                        Wind Speed: {weather.current.wind.speed} m/s
+                                    </Typography>
+                                </Box>
+
+                            </Box>
+                        </Box>
+                    </Grid>
+                    {/* {weather.current.coord && (
+                        <Grid item xs={12} lg={6} sx={{ width: '500px', height: '280px', }}>
+                            <MapContainer center={[weather.current.coord.lat, weather.current.coord.lon]} zoom={10} style={{ width: '100%', height: '100%' }}>
+                                <TileLayer
+                                    url={`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${apiKey}`}
+                                // attribution='&copy; <a href="https://www.openweathermap.org/">OpenWeatherMap</a>'
+                                />
+                            </MapContainer>
+                        </Grid>
+                    )} */}
+
+                    <Grid item xs={12} lg={12} >
+
+                        <Box sx={{ display: 'flex', flexDirection: 'row', }}>
+                            {weather.forecast.map((item, index) => (
+                                <Box key={index}
+                                    sx={{
+                                        textAlign: 'center',
+                                        // minWidth: '15%',
+                                        backgroundColor: '#fff',
+                                        padding: 3,
+                                        boxShadow: 2
+                                    }}>
                                     <div>{toDateFunction(item.dt)}</div>
                                     <img
                                         src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
@@ -149,8 +189,8 @@ const Weather = ({ farm }) => {
                                 </Box>
                             ))}
                         </Box>
-                    </div>
-                </Box>
+                    </Grid>
+                </Grid>
             )}
         </Box>
     );
