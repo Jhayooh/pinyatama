8719062b1a8b3dc33acc6500999f25455f6dac9a
baisaarnina1208@gmail.com <baisaarnina1208@gmail.com>
2024-08-04 22:13:55 +0800

    updated login UI pacheck

--- src/component/Login.js
+++ src/component/Login.js
@@ -61,87 +61,98 @@ export default function Login() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${BgImage})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: '130% 130%',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              padding:4
-            }}
-          >
-            <img src={require('./image_src/pinyatamap-logo.png')} style={{ width: '20%', height: '20%' }} />
-            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar> */}
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                label="Email"
-                color="secondary"
-                value={email}
-                onChange={(event) => { setEmail(event.target.value) }}
-                InputProps={{ style: { borderColor: 'green' } }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                label="Password"
-                color="secondary"
-                value={password}
-                onChange={(event) => {
-                  setPassword(event.target.value)
-                  console.log('pass')
-                }}
-                InputProps={{ style: { borderColor: 'green' } }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, backgroundColor: 'green' }}
-                onClick={handleLogin}
-              >
-                Login
-              </Button>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
+    <Box sx={{ backgroundColor: 'green' }}>
+      <Grid>
+        <Grid item>
+          <TextField
+          placeholder='jdhja'/>
+        </Grid>
+        <Grid item>
+
         </Grid>
       </Grid>
-    </ThemeProvider>
+    </Box>
+    // <ThemeProvider theme={defaultTheme}>
+    //   <Grid container component="main" sx={{ height: '100vh' }}>
+    //     <CssBaseline />
+    //     <Grid
+    //       item
+    //       xs={false}
+    //       sm={4}
+    //       md={7}
+    //       sx={{
+    //         backgroundImage: `url(${BgImage})`,
+    //         backgroundRepeat: 'no-repeat',
+    //         backgroundColor: (t) =>
+    //           t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+    //         backgroundSize: '130% 130%',
+    //         backgroundPosition: 'center',
+    //       }}
+    //     />
+    //     <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+    //       <Box
+    //         sx={{
+    //           my: 8,
+    //           mx: 4,
+    //           display: 'flex',
+    //           flexDirection: 'column',
+    //           alignItems: 'center',
+    //           padding:4
+    //         }}
+    //       >
+    //         <img src={require('./image_src/pinyatamap-logo.png')} style={{ width: '20%', height: '20%' }} />
+    //         {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+    //           <LockOutlinedIcon />
+    //         </Avatar> */}
+    //         <Typography component="h1" variant="h5">
+    //           Sign in
+    //         </Typography>
+    //         <Box component="form" sx={{ mt: 1 }}>
+    //           <TextField
+    //             margin="normal"
+    //             required
+    //             fullWidth
+    //             id="email"
+    //             name="email"
+    //             autoComplete="email"
+    //             autoFocus
+    //             label="Email"
+    //             color="secondary"
+    //             value={email}
+    //             onChange={(event) => { setEmail(event.target.value) }}
+    //             InputProps={{ style: { borderColor: 'green' } }}
+    //           />
+    //           <TextField
+    //             margin="normal"
+    //             required
+    //             fullWidth
+    //             name="password"
+    //             type="password"
+    //             id="password"
+    //             autoComplete="current-password"
+    //             label="Password"
+    //             color="secondary"
+    //             value={password}
+    //             onChange={(event) => {
+    //               setPassword(event.target.value)
+    //               console.log('pass')
+    //             }}
+    //             InputProps={{ style: { borderColor: 'green' } }}
+    //           />
+    //           <Button
+    //             type="submit"
+    //             fullWidth
+    //             variant="contained"
+    //             sx={{ mt: 3, mb: 2, backgroundColor: 'green' }}
+    //             onClick={handleLogin}
+    //           >
+    //             Login
+    //           </Button>
+    //           <Copyright sx={{ mt: 5 }} />
+    //         </Box>
+    //       </Box>
+    //     </Grid>
+    //   </Grid>
+    // </ThemeProvider>
   );
 }
--- src/component/page/SideNav.js
+++ src/component/page/SideNav.js
@@ -870,6 +870,7 @@ export default function SideNav() {
                 <ListItemText primary='Farms' sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
+            <Divider sx={{color:'orange', border:1, marginTop: open ? 10 : 27}}/>
             <Box sx={{ flexGrow: 0 , 
               //marginTop: open ? '50%' : '100%', display: open ? 'flex' : 'none', justifyContent:'flex-end', alignItems:'flex-end'
               }}>
