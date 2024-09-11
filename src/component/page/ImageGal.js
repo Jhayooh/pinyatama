a409ac8ba8b231357da4467e6f3434295a563edb
unknown <baisaarnina1208@gmail.com>
2024-09-11 13:00:20 +0800

    para sayo to glendel sana masaya ka

--- src/component/page/ImageGal.js
+++ src/component/page/ImageGal.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { storage } from '../../firebase/Config';
 import { ref, listAll, getDownloadURL } from 'firebase/storage';
 import './ImageGal.css';
-
-import { Gallery } from "react-grid-gallery";
+import { Gallery } from 'react-grid-gallery';
 
 const ImageGal = () => {
     const [images, setImages] = useState([]);
@@ -11,18 +10,17 @@ const ImageGal = () => {
     useEffect(() => {
         const fetchImagesRecursively = async (listRef) => {
             try {
-                // const result = await list(listRef, { maxResults: 2 });
                 const result = await listAll(listRef);
-
-                // Shuffle the array of items randomly
                 const shuffledItems = result.items.sort(() => 0.5 - Math.random());
-
-                // Fetch more than 10 random images
                 const randomItems = shuffledItems.slice(0, 20);
 
                 const imagePromises = randomItems.map(async (itemRef) => {
                     const downloadURL = await getDownloadURL(itemRef);
-                    return { src: downloadURL, width: '100%', height: 'auto' };
+                    return {
+                        src: downloadURL,
+                        width: 800, // Fixed width
+                        height: 800, // Fixed height
+                    };
                 });
 
                 const subdirectoryPromises = result.prefixes.map((folderRef) => {
@@ -42,9 +40,7 @@ const ImageGal = () => {
         const fetchImages = async () => {
             const rootRef = ref(storage, 'FarmImages');
             const allImages = await fetchImagesRecursively(rootRef);
-
-            // Select 10 random images from the fetched images
-            const selectedImages = allImages.sort(() => .5 - Math.random()).slice(0, 10);
+            const selectedImages = allImages.sort(() => 0.5 - Math.random()).slice(0, 10);
 
             setImages(selectedImages);
         };
@@ -53,7 +49,7 @@ const ImageGal = () => {
     }, [storage]);
 
     return (
-        <Gallery images={images} margin={8}  />
+        <Gallery images={images} margin={8} />
     );
 }
 
